04f200a17ca45cae788737bb21f11e62b8520ae8
Flypsi <github@flyp.si>
2025-10-13 16:34:04 +0200

    Avatar not grayscale anymore

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -25,11 +25,11 @@
 				<Breadcrumb.Root>
 					<Breadcrumb.List>
 						<Breadcrumb.Item class="hidden md:block">
-							<Breadcrumb.Link href="#">Building Your Application</Breadcrumb.Link>
+							<Breadcrumb.Link href="#">Schatzmeister</Breadcrumb.Link>
 						</Breadcrumb.Item>
 						<Breadcrumb.Separator class="hidden md:block" />
 						<Breadcrumb.Item>
-							<Breadcrumb.Page>Data Fetching</Breadcrumb.Page>
+							<Breadcrumb.Page>Dashboard</Breadcrumb.Page>
 						</Breadcrumb.Item>
 					</Breadcrumb.List>
 				</Breadcrumb.Root>
