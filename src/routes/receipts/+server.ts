51fa3ede4fa769f0ec9d88217e2fb3989f397f57
Flypsi <github@flyp.si>
2025-10-17 01:48:20 +0200

    Added CORS header to server

--- src/routes/receipts/+server.ts
+++ src/routes/receipts/+server.ts
@@ -1,10 +1,10 @@
 export async function POST({ params, request }) {
+	console.log('New Request', params, request.headers);
 
-    console.log("New Request", params, request.headers);
-
-    return new Response(JSON.stringify({x: "hallo"}), {
-        headers: {
-            "Content-Type": "application/json"
-        }
-    });
+	return new Response(JSON.stringify({ x: 'hallo' }), {
+		headers: {
+			'Content-Type': 'application/json',
+			'Access-Control-Allow-Origin': '*'
+		}
+	});
 }
