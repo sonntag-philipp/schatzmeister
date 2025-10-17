ed99a5f0c10de0f1f7a980cca199de0bc1078723
Flypsi <github@flyp.si>
2025-10-17 03:33:05 +0200

    Updated server

--- src/routes/receipts/+server.ts
+++ src/routes/receipts/+server.ts
@@ -2,8 +2,9 @@ import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
 export const POST: RequestHandler = async ({ params, request }) => {
-	console.log(request);
+    console.log(request)
 	console.log(params);
+    console.log(await request.json());
 
 	return json({ message: 'Alles klar!' }, { status: 200 });
 };
