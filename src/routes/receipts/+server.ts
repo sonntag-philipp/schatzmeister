7069d69c78440ec0d5d7f5a2c55a4334fc7f057c
Flypsi <github@flyp.si>
2025-10-17 03:51:11 +0200

    formData

--- src/routes/receipts/+server.ts
+++ src/routes/receipts/+server.ts
@@ -4,7 +4,7 @@ import type { RequestHandler } from './$types';
 export const POST: RequestHandler = async ({ params, request }) => {
     console.log(request)
 	console.log(params);
-    console.log(await request.json());
+	console.log(await request.formData());
 
 	return json({ message: 'Alles klar!' }, { status: 200 });
 };
