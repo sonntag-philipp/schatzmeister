6c53c11c43817b06fa3adaa7812a49c7f9e64412
Flypsi <github@flyp.si>
2025-10-17 13:34:05 +0200

    Tested new workflow

--- src/routes/receipts/+server.ts
+++ src/routes/receipts/+server.ts
@@ -1,10 +1,8 @@
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
-export const POST: RequestHandler = async ({ params, request }) => {
-    console.log(request)
-	console.log(params);
-	console.log(await request.formData());
+export const POST: RequestHandler = async ({ request }) => {
+	console.log(request);
 
-	return json({ message: 'Alles klar!' }, { status: 200 });
+	return json({}, { status: 200 });
 };
