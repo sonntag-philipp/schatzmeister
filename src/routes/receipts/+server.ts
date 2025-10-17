8c14c9af57b6cf05fb848fb28a1e7094d05054ad
Flypsi <github@flyp.si>
2025-10-17 03:21:11 +0200

    Updated Post

--- src/routes/receipts/+server.ts
+++ src/routes/receipts/+server.ts
@@ -1,27 +1,9 @@
+import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
 export const POST: RequestHandler = async ({ params, request }) => {
-    console.log(request);
-    console.log(params);
+	console.log(request);
+	console.log(params);
 
-	return new Response(JSON.stringify({ x: 'hallo' }), {
-		headers: {
-			'Access-Control-Allow-Origin': '*', // oder nur deine Domain
-			'Access-Control-Allow-Methods': 'POST, OPTIONS',
-			'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-			'Access-Control-Max-Age': '86400' // optional, Cache für Preflight
-		}
-	});
-}
-
-export const OPTIONS: RequestHandler = async () => {
-	return new Response(null, {
-		status: 204, // No Content
-		headers: {
-			'Access-Control-Allow-Origin': '*', // oder nur deine Domain
-			'Access-Control-Allow-Methods': 'POST, OPTIONS',
-			'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-			'Access-Control-Max-Age': '86400' // optional, Cache für Preflight
-		}
-	});
+	return json({ message: 'Alles klar!' }, { status: 200 });
 };
