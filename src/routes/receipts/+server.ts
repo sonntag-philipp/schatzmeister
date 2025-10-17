0852cf18f98542d155686d4165d2a9d82dc8f14b
Flypsi <github@flyp.si>
2025-10-17 02:09:41 +0200

    Added Options

--- src/routes/receipts/+server.ts
+++ src/routes/receipts/+server.ts
@@ -1,10 +1,30 @@
-export async function POST({ params, request }) {
-	console.log('New Request', params, request.headers);
+import type { RequestHandler } from './$types';
+
+export const POST: RequestHandler = async ({ params, request }) => {
+	console.log('New Request', {
+		...request,
+		params,
+		body: await request.json()
+	});
 
 	return new Response(JSON.stringify({ x: 'hallo' }), {
 		headers: {
-			'Content-Type': 'application/json',
-			'Access-Control-Allow-Origin': '*'
+			'Access-Control-Allow-Origin': '*', // oder nur deine Domain
+			'Access-Control-Allow-Methods': 'POST, OPTIONS',
+			'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+			'Access-Control-Max-Age': '86400' // optional, Cache für Preflight
 		}
 	});
 }
+
+export const OPTIONS: RequestHandler = async () => {
+	return new Response(null, {
+		status: 204, // No Content
+		headers: {
+			'Access-Control-Allow-Origin': '*', // oder nur deine Domain
+			'Access-Control-Allow-Methods': 'POST, OPTIONS',
+			'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+			'Access-Control-Max-Age': '86400' // optional, Cache für Preflight
+		}
+	});
+};
